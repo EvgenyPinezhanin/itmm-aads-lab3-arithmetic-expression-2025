00526b7a2fa910708008db2e068b52dde552f916
Sohibjon <sohibjonmamadjonov06@gmail.com>
2025-12-09 10:10:03 +0300

    q

--- solutions/MamadjonovS/include/tstack.h
+++ solutions/MamadjonovS/include/tstack.h
@@ -7,6 +7,8 @@
 #ifndef __TStack_H__
 #define __TStack_H__
 
+#include <stdexcept> // Для исключений
+
 const int DefaultMemSize = 1024; // стандартный размер памяти для стека
 
 template <typename T>
@@ -19,45 +21,63 @@ protected:
 
     int GetNextIndex(int index) // получить следующий индекс
     {
-
+        return index + 1; // Просто увеличиваем индекс на 1
     }
 
 public:
     TStack(int Size = DefaultMemSize) : MemSize(Size)
     {
-
+        if (Size <= 0) {
+            MemSize = DefaultMemSize; // Используем значение по умолчанию
+        }
+        pMem = new T[MemSize]; // Выделяем память
+        DataCount = 0; // Стек пуст
+        Hi = -1; // Индекс вершины -1 (стек пуст)
     }
 
-    ~Tstack()
+    ~TStack() // Исправлено: было Tstack, должно быть TStack
     {
-
+        delete[] pMem; // Освобождаем память
+        pMem = nullptr; // Обнуляем указатель
     }
-
 
     bool IsEmpty() const
     {
-        return false;
+        return DataCount == 0; // Стек пуст, если количество элементов 0
     }
 
     bool IsFull() const
     {
-        return false;
+        return DataCount == MemSize; // Стек полон, если количество элементов равно размеру
     }
 
-
-    void Put(const T &Val) // добавить значение
+    void Put(const T& Val) // добавить значение
     {
-    
+        if (IsFull()) {
+            throw std::overflow_error("Stack is full"); // Ошибка переполнения
+        }
+        Hi++; // Увеличиваем индекс вершины
+        pMem[Hi] = Val; // Записываем значение
+        DataCount++; // Увеличиваем счетчик элементов
     }
 
     T Top() const // получить значение
     {
-        return T{};
-    }   
+        if (IsEmpty()) {
+            throw std::underflow_error("Stack is empty"); // Ошибка пустого стека
+        }
+        return pMem[Hi]; // Возвращаем значение с вершины
+    }
 
     void Get() // извлечь значение
     {
-
+        if (IsEmpty()) {
+            throw std::underflow_error("Stack is empty"); // Ошибка пустого стека
+        }
+        // Для простых типов T дополнительных действий не нужно
+        // Для сложных типов может потребоваться вызов деструктора
+        Hi--; // Уменьшаем индекс вершины
+        DataCount--; // Уменьшаем счетчик элементов
     }
 };
 
--- solutions/MamadjonovS/samples/sample_expression.cpp
+++ solutions/MamadjonovS/samples/sample_expression.cpp
@@ -7,6 +7,8 @@
 #include <iostream>
 #include <string>
 
+#include "tstack.h"
+
 #include "texpressionanalyzer.h"
 
 void main()
